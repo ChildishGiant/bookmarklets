f5ec09f8c2e2e408a90a39ad38674f5a01f59cfa
SolarPolarMan <solarpolarman@gmail.com>
2016-06-23 14:15:31 +0100

    start.

---
+++ jssource.js
@@ -0,0 +1,21 @@
+
+$( document ).ready(function() {
+
+  #setting buttons content
+  if ($("#wbm")) {
+    $("#wbm").attr("href", "javascript:location.replace(\"http://web.archive.org/web/0/\"+window.location.href);")
+  }
+
+  if ($("#lenny")) {
+  ]
+  
+                javascript: (function () {
+                  var jsCode = document.createElement('script');
+                  jsCode.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.12/clipboard.min.js');
+                  document.body.appendChild(jsCode);
+                  new Clipboard('.btn');
+
+ }());
+  }
+
+})
